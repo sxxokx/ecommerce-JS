ca50e063d7f60882352675f0669372d8973a6e73
Jean <jean.angelo.borges@gmail.com>
2022-09-14 15:11:35 -0300

    entrega

--- js/database.js
+++ js/database.js
@@ -1,64 +1,50 @@
-// Banco de dados dos produtos
-
 const data = [
   {
-    id: 1,
-    img: "../img/jaqueta.svg",
-    nameItem: "Lightweight Jacket",
-    description:
-      "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+      id: 0,
+      tag: 'Hardware',
+      image: "../img/gtx asus 1650.jpg",
+      title: 'Gtx 1650',
+      desc: 'Placa de video de ótimo desempenho...',
+      price: 529.99
   },
   {
-    id: 2,
-    img: "../img/gorro.svg",
-    nameItem: "Black Hat",
-    description:
-      "O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Acessórios"],
+      id: 1,
+      tag: 'Hardware',
+      image: '../img/ram asgard.webp',
+      title: 'Ram DDR4',
+      desc: 'Cada memoria com 8gb e 3200 Mhz...',
+      price: 174.89
   },
   {
-    id: 3,
-    img: "../img/mascara.svg",
-    nameItem: "Mask",
-    description:
-      "Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
-    value: 40,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Acessórios"],
+      id: 2,
+      tag: 'Periféricos',
+      image: '../img/tecado infernal.webp',
+      title: 'Infernal Dragon',
+      desc: 'Teclado Viserion, swith opitco...',
+      price: 269.99 
   },
   {
-    id: 4,
-    img: "../img/camiseta_preta.svg",
-    nameItem: "T-Shirt",
-    description:
-      "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+      id: 3,
+      tag: 'Periféricos',
+      image: '../img/mouse ryu.webp',
+      title: 'Infernal Dragon Ryu',
+      desc: 'Mouse de Exelente desempenho...',
+      price: 149.99
   },
   {
-    id: 5,
-    img: "../img/camiseta_branca.svg",
-    nameItem: "Short-Sleeve T-Shirt",
-    description:
-      "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+      id: 4,
+      tag: 'Notebooks',
+      image: './img/lenovo-legion-y540.webp',
+      title: 'Legion y540',
+      desc: 'Notebook de alto desempenho...',
+      price: 930.29
   },
   {
-    id: 6,
-    img: "../img/moletom.svg",
-    nameItem: "Champion Packable Jacket",
-    description:
-      "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
-  },
-];
+      id: 5,
+      tag: 'Notebooks',
+      image: '../img/acer nitro.webp',
+      title: 'Acer Nitro 5',
+      desc: 'Notebook de alto desempenho...',
+      price: 989.89
+  }
+]
---
+++ js/index.js
@@ -0,0 +1,94 @@
+const cart = []
+function listProducts(database) {
+    document.querySelector('.cards').innerHTML = ''
+    for (let i in database)
+        document.querySelector('.cards').insertAdjacentHTML('beforeend', `
+            <li id="${database[i].id}">            
+                <figure> 
+                    <input class="categor" type="button" value="${database[i].tag}">             
+                    <img src="${database[i].image}" alt="">
+                </figure>              
+                <section>
+                    <h1>${database[i].title}</h1>
+                    <p class="desc">${database[i].desc}</p>
+                    <p class="price">R$${database[i].price.toFixed(2).toString().replace('.', ',')}</p>
+                    <button onclick="addClick(event)">Adicionar ao carrinho</button>
+                </section>
+            </li>
+            `)
+            
+}
+listProducts(data)
+
+function listCart() {
+    document.querySelector('.cart').innerHTML = ''
+    let total = 0
+    let quantidade = 0
+    for (let i in cart) {
+        let productPrice = cart[i].price.toFixed(2).toString().replace('.', ',')
+        if (cart[i].count > 1) { productPrice += ' x' + cart[i].count }
+        total += cart[i].price * cart[i].count
+        quantidade += cart[i].count
+        document.querySelector('.cart').insertAdjacentHTML('beforeend', `
+        <li id="${cart[i].id}">
+            <figure>
+                <img src="${cart[i].image}" alt="">
+            </figure>
+            <section>
+                <h2>${cart[i].title}</h2>
+                <p class="price">R$${productPrice}</p>
+                <button onclick="removeClick(event)">Remover Produto</button>
+            </section>
+        </li>
+    `)
+    }
+    qtd.innerText = quantidade
+    val.innerText = 'R$' + total.toFixed(2).toString().replace('.', ',')
+}
+
+
+function addClick(event) {
+    //Recebe o índice do produto selecionado
+    let index = (event.target.parentNode.parentNode.id)
+    //Localiza o produto no database
+    const product = data.find(ff => ff.id == index)
+
+    //Adiciona novo produto ao array do carrinho ou soma quantidade se já existir   
+    let j = -1
+    cart.forEach((ff, i) => ff.id == product.id ? j = i : false)
+    if (j > -1)
+        cart[j]['count']++
+    else {
+        product['count'] = 1
+        cart.push(product)
+    }
+
+    //Lista o carrinho
+    listCart()
+}
+
+function removeClick(event) {
+    //Recebe o índice do produto selecionado
+    let index = -1
+    cart.forEach((ff, i) => ff.id == event.target.parentNode.parentNode.id ? index = i : false)
+
+    //Remove os produtos conforme quantidade
+    cart[index].count > 1 ? cart[index].count-- : cart.splice(index, 1)
+
+    //Lista o carrinho
+    listCart()
+}
+
+//Categoria por tags
+document.querySelector('.tagMenu').addEventListener('click', function (event) {
+    if (event.target.tagName == 'A') {
+        if (event.target.innerText == 'TODOS OS PRODUTOS')
+            listProducts(data)
+        else
+            listProducts(data.filter(ff => ff.tag.toUpperCase() == event.target.innerText))
+    }
+})
+
+function searchClick() {
+   listProducts(data.filter(ff => ff.title.toLowerCase().includes(searchbox.value.toLowerCase())))
+}
